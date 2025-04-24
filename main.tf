b52e4b6b5668713a35f395a577dff526473cb874
StiflerMasuku <sifiso.masuku@dimensiondata.com>
2025-04-24 12:20:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,3 +71,54 @@ resource "aws_iam_role_policy_attachment" "Queue_Statistics_Attach" {
     role = aws_iam_role.Queue_Statistics.name
     policy_arn = aws_iam_policy.Queue_Statistics_Policy.arn
 }
+
+
+
+
+
+resource "aws_iam_role" "ASCM" {
+  name = "ASCM"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+          }
+          },
+          ]
+      })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+data "aws_iam_policy_document" "ASCM_Policy" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:DeleteObject",
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:GenerateDataKey"
+      ]
+      resources = ["*"]
+    }
+}
+
+resource "aws_iam_policy" "ASCM_Policy" {
+  name = "ASCM_Policy"
+  description = "Policy for ASCM"
+  policy = data.aws_iam_policy_document.ASCM_Policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ASCM_Attach" {
+  role = aws_iam_role.ASCM.name
+  policy_arn = aws_iam_policy.ASCM_Policy.arn
+}
